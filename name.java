260c36bc95db7c8a218bc0958ad29909d5bab697
21311161js <21311161js@tama.ac.jp>
2014-10-08 16:28:29 +0900

    第三回プログラム

---
+++ name.java
@@ -0,0 +1,13 @@
+ï»¿class name
+{
+ public static void main(String argv{})
+ {
+  string name;
+  System.out.println("‚ ‚È‚½‚Ì–¼‘O‚ÍH");
+  System.out.print("name=");
+  name = new java.util.Scanner(System.in)nextLine();
+  System.out.println(name+"‚³‚ñA‚±‚ñ‚É‚¿‚Í");
+ }
+
+
+}
