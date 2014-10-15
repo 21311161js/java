4f2c84b820269ba7346e5b37f8004fcc0c2fc12d
21311161js <21311161js@tama.ac.jp>
2014-10-15 16:14:23 +0900

    第四回計算プログラム

---
+++ keisan.java
@@ -0,0 +1,24 @@
+class keisan{
+public static void main(String args[])
+{
+System.out.println("hello world");
+// “ü—Í:2‚Â‚Ì”’l‚ğ“ü—Í
+//String •Ï” = new java.util.scanner(System.in).nextLine();
+//int •Ï” = new java.util.Scanner(System.in).nextInt();
+int in0 = new java.util.Scanner(System.in).nextInt();
+int in1 = new java.util.Scanner(System.in).nextInt();
+//ˆ—:‘«‚µZ
+int add = (in0+in1);
+//ˆ—:ˆø‚«Z
+int add = (in0-in1);
+//ˆ—:Š|‚¯Z
+int add = (in0*in1);
+//ˆ—:Š„‚èZ
+int add = (in0/in1);
+//ˆ—:—]‚è
+int add = (in0%in1);
+
+//ŒvZŒ‹‰Ê‚ğo—Í
+System.out.println("‘«‚µZ"+add);
+}
+}
