a11d1903c9e4abd95e7f6b94e18df556e9e5d32e
21311161js <21311161js@tama.ac.jp>
2014-11-26 16:11:46 +0900

    第八回

--- suuretu.java
+++ suuretu.java
@@ -1,17 +1,20 @@
-#java double 桁落ち
+//java double 桁落ち
+import java.math.*;
 class suuretu{
 public static void main(String args[])
 {
-double [] f = new double[101];
-f[0] = 0;
+BigInteger ryuka = new BigInteger("0");
+BigInteger fibo = new BigInteger("0");
+BigInteger [] f = new BigInteger[101];
+f[0] = new BigInteger("0");
 System.out.println("フィボナッチ数列");
 for (int i=0 ; i<=100 ; i++){
 if (i == 0){
-f[0] = 0;
+f[0] = new BigInteger("0");
 System.out.println(f[0]);
 }
 if (i == 1){
-f[1] = 1;
+f[1] = new BigInteger("1");
 System.out.println(f[1]);
 }
 if (i >= 2){
@@ -19,15 +22,15 @@ f[i] = f[i-1] + f[i-2];
 System.out.println(f[i]);
 }
 }
-f[0] = 0;
+f[0] = new BigInteger("0");
 System.out.println("リュカ数列");
 for (int i=0 ; i<=100 ; i++){
 if (i == 0){
-f[0] = 2;
+f[0] = new BigInteger("2");
 System.out.println(f[0]);
 }
 if (i == 1){
-f[1] = 1;
+f[1] = new BigInteger("1");
 System.out.println(f[1]);
 }
 if (i >= 2){
